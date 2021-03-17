889787a339a8493be297e3b47069c7add5f1cbc5
tachikoma-chan <nieve33sn0wmyyy@gmail.com>
2021-03-17 18:09:59 +0000

    add favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :favorites, dependent: :destroy
+  has_many :like_users, through: :favorites, source: :user
+  
   validates :content, presence: true, length: { maximum: 255 }
 end
